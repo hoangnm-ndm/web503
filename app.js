9f103475e7af19c35d5815365624dbb0f0bcfa8e
hoangnm <hoangnm.ndm@gmail.com>
2023-03-09 05:52:45 +0700

    lession 2

--- app.js
+++ app.js
@@ -1,29 +1,97 @@
-const express = require("express");
+const products = [
+  { id: 1, name: "Iphone 12", price: 2000 },
+  { id: 2, name: "Iphone 11000", price: 1000 },
+];
+
+// Cài đặt nodejs, default đã có rất package hỗ trợ
+
+// cú pháp ES5
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+
+//     products.push({ name: "Iphone 10", price: 500 });
+//     // routing
+//     if (req.url == "/") {
+//         res.setHeader("Content-Type", "text/html");
+//         res.end(`<html>
+//                     <body>
+//                         <form action="/products" method="post">
+//                             <input type="text" name="name" />
+//                             <button>Submit</button>
+//                         </form>
+//                     </body>
+//                 </html>`);
+//     }
+//     if (req.url == "/products" && req.method == "POST") {
+//         const body = [];
+//         req.on("data", function (chunk) {
+//             body.push(chunk);
+//         });
+//         req.on("end", function () {
+//             const bodyParse = Buffer.concat(body).toString();
+//             console.log(bodyParse);
+//         });
+//         res.end(JSON.stringify(products));
+//     }
+// });
+// server.listen(8080, () => {
+//     console.log("Server is running on port 8080");
+// });
+
+// npm init -y
+// npm i nodemon -D
+// "start": "nodemon app.js"
+
+import express from "express";
+
 const app = express();
-const port = 8000;
 
-app.get("/", (req, res) => {
-  // code lai noi dung ben server.js
-  res.send(`
-    <h1>Home Page</h1>
-    <h2>Noi dung cua Homepage</h2>
-  `);
-});
+app.use(express.json());
+
+// Trả về danh sách
 app.get("/products", (req, res) => {
-  // code lai noi dung ben server.js
-  res.send(`
-    <h1>Products Page</h1>
-  `);
+  res.json(products);
+});
+// Trả về một sản phẩm
+app.get("/products/:id", (req, res) => {
+  const id = req.params.id;
+  const product = products.find((product) => product.id == id);
+  res.json(product);
+});
+// Thêm sản phẩm
+app.post("/products", (req, res) => {
+  // Lấy dữ liệu từ client body gửi lên
+  const product = req.body;
+  // push vào mảng products
+  products.push(product);
+  // trả về client dạng json
+  res.json({
+    message: "Thêm sản phẩm thành công",
+    products,
+  });
+});
+// Xóa sản phẩm
+app.delete("/products/:id", (req, res) => {
+  const id = req.params.id;
+  const newProducts = products.filter((item) => item.id != id);
+  res.status(200).json({
+    message: "Xóa sản phẩm thành công",
+    products: newProducts,
+  });
+});
+// Cập nhật sản phẩm
+
+app.patch("/products/:id", (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  const newProducts = products.map((item) => (item.id == id ? body : item));
+  res.status(200).json({
+    message: "Cập nhật sản phẩm thành công",
+    products: newProducts,
+  });
 });
 
-app.get("/product/:id", (req, res) => {
-  res.send(
-    `
-    <h1>Product có id la: ${req.params.id}</h1>
-    
-    `
-  );
-});
-app.listen(port, () => {
-  console.log(`ung dung dang chay vao file app tren port: ${port}`);
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
 });
