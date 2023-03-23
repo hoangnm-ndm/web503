c0b895f8f5fce5be222173099c2bf298b6aa7651
hoangnm <hoangnm.ndm@gmail.com>
2023-03-23 14:24:37 +0700

    add vite joi and mongoose

--- src/app.js
+++ src/app.js
@@ -1,15 +1,25 @@
 import express from "express";
 import routerProduct from "./routes/product.js";
+import mongoose from "mongoose";
+
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
+mongoose.connect("mongodb://127.0.0.1:27017/test");
+const Cat = mongoose.model("Cat", { name: String });
+
+const kitty = new Cat({ name: "Zildjian" });
+kitty.save().then(() => console.log("meow"));
+
 app.use("/api", routerProduct);
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+
+export const viteNodeApp = app;
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server is running on port ${process.env.PORT}`);
+// });
 
 // install axios.
 // install concurrently
