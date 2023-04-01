2b8c560865f6c029f35e64f8d4dfc60183228c22
hoangnm <hoangnm.ndm@gmail.com>
2023-04-01 17:04:13 +0700

    add step

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,32 +1,16 @@
-import dotenv from "dotenv";
-import Product from "../models/product.js";
-import { userSchema } from "../schemas/auth.js";
-
-dotenv.config();
-
-export const signup = async (req, res) => {
-  try {
-    const { error } = userSchema.validate(req.body);
-    if (error) {
-      return res.status(400).json({
-        message: error.details[0].message,
-      });
-    }
-
-    const product = await Product.create(req.body);
-
-    if (!product) {
-      res.send({
-        messenger: "Thêm sản phẩm thất bại",
-      });
-    }
-    return res.status(200).json({
-      message: "Thêm sản phẩm thành công",
-      data: product,
-    });
-  } catch (error) {
-    res.status(400).send({
-      messenger: error,
-    });
-  }
-};
+/**
+ * Với signup
+ * B1: Validate req.body với Joi
+ * B2: Lấy từ req.body ra name, email, password.
+ * B3: Kiển tra xem email này đã tồn tại trong db hay chưa?
+ * B4: Mã hoá với bcryptjs
+ * B5: Lưu user vào DB
+ * B6: Gán password = undefined.
+ * B7: Trả về message, accessToken và user cho người dùng
+ *
+ * Với signin:
+ * B1: Validate req.body
+ * B2: Lấy email và password ra lần lượt kiểm tra từng giá trị.
+ * B3: Nếu đúng cả password và email -> tạo ra ra token.
+ * B4: Gán password = undefined trước khi trả dữ liệu cho người dùng.
+ */
