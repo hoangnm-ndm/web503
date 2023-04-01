3abb155887ebacb49660977c3e75360c76982f5e
hoangnm <hoangnm.ndm@gmail.com>
2023-04-01 15:08:26 +0700

    add step

--- src/middlewares/checkPermission.js
+++ src/middlewares/checkPermission.js
@@ -3,6 +3,15 @@ import User from "../models/user.js";
 import dotenv from "dotenv";
 dotenv.config();
 const { SECRET_CODE } = process.env;
+
+/**
+ * B1: Kiem tra req.headers.authorization co ton tai hay khong?
+ *    -> Nếu tồn tại -> lấy ra token.
+ * B2: Dùng jwt.verify để lấy ra payload, nếu error -> trả về error với từng dạng lỗi của token.
+ * B3: Từ id có trong payload của token -> lấy ra user.
+ * B4: Từ user -> check quyền role của user. Nếu có quyền admin -> next.
+ *  -> Nếu không có quyền admin thì throw error
+ */
 export const checkPermission = async (req, res, next) => {
   const token =
     req.headers.authorization && req.headers.authorization.split(" ")[1];
