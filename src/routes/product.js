016a5146e51ea328070b52e314a1190a14ef67d9
hoangnm <hoangnm.ndm@gmail.com>
2023-04-06 17:40:34 +0700

    update

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,16 +1,10 @@
 import express from "express";
-import {
-  getAll,
-  getDetail,
-  create,
-  update,
-  remove,
-} from "../controllers/product.js";
+import { getAll, get, create, update, remove } from "../controllers/product.js";
 import { checkPermission } from "../middlewares/checkPermission.js";
 const router = express.Router();
 
 router.get("/", getAll);
-router.get("/:id", getDetail);
+router.get("/:id", get);
 router.post("/", checkPermission, create);
 router.put("/:id", checkPermission, update);
 router.delete("/:id", checkPermission, remove);
