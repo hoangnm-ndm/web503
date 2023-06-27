0350bece569459cfebb7ad611311b16039ce3c19
hoangm.ndm <hoangm.ndm@gmail.com>
2023-06-27 11:17:01 +0700

    Buổi 1: router

--- app.js
+++ app.js
@@ -13,7 +13,14 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.end(`<h1>Day la trang about</h1>`);
 });
+app.get("/products", (req, res) => {
+  res.end(`<h1>Day la trang san pham</h1>`);
+});
 
+app.get("/products/:id", (req, res) => {
+  const id = req.params.id;
+  res.end(`<h1>Day la trang chi tiet san pham co id la: ${id}</h1>`);
+});
 app.listen(8088, () => {
   console.log("Server is running on port 8088");
 });
