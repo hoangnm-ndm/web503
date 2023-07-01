db45a77320afeea362b0559547379b19dcb5ac01
hoangm.ndm <hoangm.ndm@gmail.com>
2023-07-01 10:40:14 +0700

    Buổi 3: Update product

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import express from "express";
 const app = express();
 
+app.use(express.json());
+
 app.get("/products", async (req, res) => {
   try {
     const { data } = await axios.get(`http://localhost:3000/products`);
@@ -20,20 +22,51 @@ app.get("/products", async (req, res) => {
     });
   }
 });
-// app.get("/products/:id", (req, res) => {
-//   const id = req.params.id;
-//   const product = products.find((product) => product.id === id);
-//   if (!product) {
-//     return res.status(404).json({
-//       message: "Không tìm thấy sản phẩm",
-//     });
-//   }
 
-//   return res.status(200).json({
-//     message: "Hiển thị chi tiết sản phẩm thành công!",
-//     datas: product,
-//   });
-// });
+app.get("/products/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const datas = await axios.get(`http://localhost:3000/products/${id}`);
+    if (!datas || datas.status !== 200) {
+      return res.status(404).json({
+        message: "Không tìm thấy sản phẩm",
+      });
+    }
+    const { data } = datas;
+    return res.status(200).json({
+      message: "Hiển thị chi tiết sản phẩm thành công!",
+      datas: data,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+app.put("/products/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const body = req.body;
+    const { data } = await axios.put(
+      `http://localhost:3000/products/${id}`,
+      body
+    );
+    if (!data) {
+      return res.status(404).json({
+        message: "Cập nhật sản phẩm thất bại!",
+      });
+    }
+    return res.status(200).json({
+      message: "Cập nhật sản phẩm thành công!",
+      datas: data,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+});
 
 // app.delete("/products/:id", (req, res) => {
 //   const id = req.params.id;
