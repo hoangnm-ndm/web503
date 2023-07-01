d06f0c8b318687eba836ef31458e1892f2b99020
hoangm.ndm <hoangm.ndm@gmail.com>
2023-07-01 11:24:00 +0700

    Buổi 3 CRUD Done

--- app.js
+++ app.js
@@ -68,27 +68,47 @@ app.put("/products/:id", async (req, res) => {
   }
 });
 
-// app.delete("/products/:id", (req, res) => {
-//   const id = req.params.id;
-//   const product = products.find((product) => product.id === id);
-//   if (!product) {
-//     return res.status(404).json({
-//       message: "Không tìm thấy sản phẩm",
-//     });
-//   }
+app.post("/products", async (req, res) => {
+  try {
+    const body = req.body;
+    const { data } = await axios.post(`http://localhost:3000/products`, body);
+    if (!data) {
+      return res.status(404).json({
+        message: "Thêm mới sản phẩm thất bại!",
+      });
+    }
+    return res.status(200).json({
+      message: "Thêm mới sản phẩm thành công!",
+      datas: data,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+});
 
-//   const newProducts = products.filter((product) => product.id !== id);
-
-//   return res.status(200).json({
-//     message: "Xoá sản phẩm thành công!",
-//     datas: product,
-//     newProducts,
-//   });
-// });
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { status } = await axios.delete(
+      `http://localhost:3000/products/${id}`
+    );
+    if (!status || status !== 200) {
+      return res.status(404).json({
+        message: "Xoá sản phẩm thất bại!",
+      });
+    }
+    return res.status(200).json({
+      message: "Xoá sản phẩm thành công!",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+});
 
 app.listen(8088, () => {
   console.log("Server is running on port 8088");
 });
-
-// Bước 1: tạo folder -> npm init -y
-// Bước 2: cd folder -> create app.js
