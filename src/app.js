f6ad48d02ee4a43a8deae0ec34d76fd0813a5f9e
hoangm.ndm <hoangm.ndm@gmail.com>
2023-07-18 11:00:19 +0700

    edit base

--- src/app.js
+++ src/app.js
@@ -1,10 +1,6 @@
 import express from "express"
-import dotenv from "dotenv"
-dotenv.config()
 
 const app = express()
-
-app.use("/api", router)
 
 app.listen(8000, () => {
   console.log(`Server is running on 8000`)
