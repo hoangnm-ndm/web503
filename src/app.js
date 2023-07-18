b157498eae01f3eaf5218930889ab157426a66f2
hoangm.ndm <hoangm.ndm@gmail.com>
2023-07-18 07:28:42 +0700

    create base asignment 1

--- src/app.js
+++ src/app.js
@@ -1,25 +1,11 @@
 import express from "express"
 import dotenv from "dotenv"
-import router from "./routes"
-import mongoose from "mongoose"
-import cors from "cors"
-import bodyParser from "body-parser"
 dotenv.config()
-
-const { PORT, DB_URL } =  process.env
 
 const app = express()
 
-app.use(cors())
-// app.use(express.json({strict: false}))
-// app.use(bodyParser.urlencoded({ extended: true }))
-
 app.use("/api", router)
 
-mongoose.connect(`${DB_URL}`).then(() => {
-  console.log(`DB connection established!`)
-})
-
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`)
+app.listen(8000, () => {
+  console.log(`Server is running on 8000`)
 })
