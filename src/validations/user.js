adde4fe3c470d0eb5885fd2fcc8371a02d31e815
hoangm.ndm <hoangm.ndm@gmail.com>
2023-07-25 09:05:38 +0700

    Buổi 10: fix validation User

--- src/validations/user.js
+++ src/validations/user.js
@@ -4,7 +4,7 @@ export const signUpValid = Joi.object({
     userName: Joi.string().required().min(6).max(255),
     email: Joi.string().email().required(),
     password: Joi.string().required().min(6).max(255),
-    confirmPassword: Joi.string().required().min(6).max(255).valid(Joi.ref("password"))
+    confirmPassword: Joi.string().required().min(6).max(255).valid(Joi.isRef(Joi.ref("password")))
 })
 
 export const signInValid = Joi.object({
