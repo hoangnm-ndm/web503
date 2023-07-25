ae0207704ad582b430ff1df3b869943c40746a13
hoangm.ndm <hoangm.ndm@gmail.com>
2023-07-25 10:59:00 +0700

    Buổi 10: SignUp phần 2

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,18 +1,41 @@
 import { signUpValidator } from '../validations/user';
+import bcryptjs from "bcryptjs"
+import User from '../models/User'
 export const signUp = async(req, res) => {
     try {
         // Bước 1: Kiểm tra dữ liệu người dùng
         const { error } = signUpValidator.validate(req.body, {abortEarly: false})
-        console.log(error)
-        const errors = error.details.map(err => err.message)
-        ["loi a", "loi b", "loi c"]
         if(error ) {
+            const errors = error.details.map(err => err.message)
             return res.status(400).json({
                 messages: errors
             })
         }
         // Bước 2: Email người dùng đăng ký đã tồn tại trong DB hay chưa?
+        const userExist = await User.findOne({ email: req.body.email})
+        if(userExist) {
+            return res.status(400).json({
+                message: "Email này đã được đăng ký, bạn có muốn đăng nhập không?"
+            })
+        }
+
         // Bước 3: Mã hoá mật khẩu
+
+        // const hashPassword = bcryptjs.hashSync(req.body.password, 10)
+        const hashPassword = await bcryptjs.hash(req.body.password, 10)
+        
+        // Bước 4: Thông báo thành công:
+        const user = await User.create({
+            userName: req.body.userName,
+            email: req.body.email,
+            password: hashPassword
+        })
+
+        user.password = undefined
+        return res.status(200).json({
+            message: "Đăng ký thành công!",
+            user
+        })
 
     } catch (error) {
         return res.status(500).json({
