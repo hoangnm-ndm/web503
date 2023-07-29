86529fe8c36d69a790c0ec5220bc91d348621eba
hoangm.ndm <hoangm.ndm@gmail.com>
2023-07-29 08:20:04 +0700

    Buổi 12: Deploy db with mongodb atlas

--- src/app.js
+++ src/app.js
@@ -2,18 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 import router from "./routes";
 import mongoose from "mongoose";
+import cors from "cors";
 
 dotenv.config();
 const { PORT, DB_URI } = process.env;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-mongoose.connect(`${DB_URI}`)
-  .then(() => console.log('Connected!'));
+mongoose.connect(`${DB_URI}`).then(() => console.log("Connected!"));
 
-app.use("/api", router)
+app.use("/api", router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- src/middlewares/checkPermission.js
+++ src/middlewares/checkPermission.js
@@ -1,48 +1,51 @@
-import jwt from "jsonwebtoken"
-import dotenv from "dotenv"
-import User from "../models/User"
-dotenv.config()
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import User from "../models/User";
+dotenv.config();
 
-const { SECRET_KEY } = process.env
+const { SECRET_KEY } = process.env;
 
 export const checkPermission = async (req, res, next) => {
-    try {
-        // Bước 1: Kiểm tra xem headers có token hay không?
-        const token = req.headers.authorization?.split(" ")[1]
+  try {
+    // Bước 1: Kiểm tra xem headers có token hay không?
+    const token = req.headers.authorization?.split(" ")[1];
 
-        if(!token) {
-            return res.status(404).json({
-                message: "Bạn chưa đăng nhập!"
-            })
-        }
-
-        // Bước 2: Verify token để lấy user.
-        const decoded = jwt.verify(token, SECRET_KEY)
-        const user = await User.findById(decoded._id)
-        if(!user) {
-            return res.status(400).json({
-                message: "User không tồn tại trong hệ thống!"
-            })
-        }
-
-        if(user.role !== "admin") {
-            return res.status(400).json({
-                message: "Bạn không có quyền làm việc này!"
-            })
-        }
-
-        // Cach 2:
-        // if(user && user.role === "admin"){
-        //     next()
-        // }
-        // return res.status(400).json({
-        //     message: "Ban khong co quyen lam viec nao!"
-        // })
-        next()
-    } catch (error) {
-        return res.status(500).json({
-            name: error.name,
-            message: error.message
-        })
+    if (!token) {
+      return res.status(404).json({
+        message: "Bạn chưa đăng nhập!",
+      });
     }
-}
+
+    // Bước 2: Verify token để lấy user.
+    const decoded = jwt.verify(token, SECRET_KEY);
+    if (!decoded) {
+      throw new Error("JWT Error:");
+    }
+    const user = await User.findById(decoded._id);
+    if (!user) {
+      return res.status(400).json({
+        message: "User không tồn tại trong hệ thống!",
+      });
+    }
+
+    if (user.role !== "admin") {
+      return res.status(400).json({
+        message: "Bạn không có quyền làm việc này!",
+      });
+    }
+
+    // Cach 2:
+    // if(user && user.role === "admin"){
+    //     next()
+    // }
+    // return res.status(400).json({
+    //     message: "Ban khong co quyen lam viec nao!"
+    // })
+    next();
+  } catch (error) {
+    return res.status(500).json({
+      name: error.name,
+      message: error.message,
+    });
+  }
+};
