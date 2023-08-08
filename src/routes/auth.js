542ad4b44cc615c77b181e6262f014a6fb9205e8
hoangm.ndm <hoangm.ndm@gmail.com>
2023-08-08 10:44:50 +0700

    rebase

--- src/routes/auth.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { signUp } from "../controllers/auth";
-const routerAuth = express.Router();
-
-routerAuth.post("/signup", signUp);
-
-
-export default routerAuth;
