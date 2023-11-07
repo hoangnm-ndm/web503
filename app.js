622de90581e6c2ae426b6f3cbdb21f67429d655a
hoangnm.ndm <hoangnm.ndm@gmail.com>
2023-11-07 19:38:24 +0700

    day 2: express install

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
-import http from "http";
+// import http from "http";
 
-const app = http.createServer((req, res) => {
-  const { url, method } = req;
-  console.log(url, method);
-  switch (url) {
-    case "/products":
-      res.end(`<h1>Products Page</h1>`);
-      break;
-    case "/home":
-      res.end(`<h1>Home Page</h1>`);
-      break;
-  }
+// const app = http.createServer((req, res) => {
+//   const { url, method } = req;
+//   console.log({ url, method });
+
+//   // Cach 2:
+//   if (url === "/products" && method === "GET") {
+//     res.end(`<div>Products Page</div>`);
+//   }
+
+//   if (url === "/products" && method === "POST") {
+//     res.end(`<div>
+//     <form>
+//     <label>Ten san pham</label>
+//     <input placeholder="Ten san pham" />
+//     <button>Them</button>
+//     </form>
+//     </div>`);
+//   }
+// });
+
+// app.listen(8000, () => {
+//   console.log(`Server is running on PORT 8000`);
+// });
+
+import express from "express"; // module syntax
+const app = express();
+const port = 8000;
+
+app.get("/", (req, res) => {
+  res.send("Homepage!");
 });
 
-app.listen(8000, () => {
-  console.log(`Server is running on PORT 8000`);
+app.get("/shop", (req, res) => {
+  res.send("Shop page!");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About page!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
--- notes/day1.md
+++ notes/day1.md
@@ -1,6 +1,8 @@
 `npm init -y`
 `npm i`
 
+- Tạo ra service đầu tiên: app.js
+
 - vào trong package.json sửa:
 
 ```json
