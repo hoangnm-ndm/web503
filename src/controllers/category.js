054b980a7d53e60e5e277854fd38de898252d717
hoangnm.ndm <hoangnm.ndm@gmail.com>
2023-11-19 20:02:26 +0700

    CRUD category

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -3,7 +3,6 @@ import Category from "../models/Category";
 export const getAllCateries = async (req, res) => {
   try {
     const data = await Category.find();
-    console.log(data);
     if (!data || !data.length) {
       return res.status(404).json({
         message: "Khong co danh muc nao!",
@@ -12,6 +11,26 @@ export const getAllCateries = async (req, res) => {
     }
     return res.status(200).json({
       message: "Lay danh sach danh muc thanh cong!",
+      data,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      name: error.name,
+      message: error.message,
+    });
+  }
+};
+
+export const getDetailCategory = async (req, res) => {
+  try {
+    const data = await Category.findById(req.params.id);
+    if (!data) {
+      return res.status(404).json({
+        message: "Khong co danh muc nao!",
+      });
+    }
+    return res.status(200).json({
+      message: "Lay danh muc thanh cong!",
       data,
     });
   } catch (error) {
@@ -38,6 +57,56 @@ export const createCategory = async (req, res) => {
     }
     return res.status(200).json({
       message: "Tao danh muc thanh cong!",
+      data,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      name: error.name,
+      message: error.message,
+    });
+  }
+};
+
+export const removeCategory = async (req, res) => {
+  try {
+    const data = await Category.findOneAndDelete({ _id: req.params.id });
+    if (!data) {
+      return res.status(404).json({
+        message: "Xoa khong thanh cong!",
+      });
+    }
+    return res.status(200).json({
+      message: "Xoa thanh cong!",
+      data,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      name: error.name,
+      message: error.message,
+    });
+  }
+};
+
+export const updateCategory = async (req, res) => {
+  try {
+    const body = req.body;
+    if (!body) {
+      return res.status(400).json({
+        message: "Du lieu khong hop le!",
+      });
+    }
+    const data = await Category.findOneAndReplace(
+      { _id: req.params.id },
+      body,
+      { new: true }
+    );
+    if (!data) {
+      return res.status(404).json({
+        message: "Cap nhat danh muc that bai!",
+      });
+    }
+    return res.status(200).json({
+      message: "Cap nhat danh muc thanh cong!",
       data,
     });
   } catch (error) {
--- src/routes/category.js
+++ src/routes/category.js
@@ -1,9 +1,18 @@
 import { Router } from "express";
-import { createCategory, getAllCateries } from "../controllers/category";
+import {
+  createCategory,
+  getAllCateries,
+  getDetailCategory,
+  removeCategory,
+  updateCategory,
+} from "../controllers/category";
 
 const categoryRouter = Router();
 
 categoryRouter.get("/", getAllCateries);
 categoryRouter.post("/", createCategory);
+categoryRouter.get("/:id", getDetailCategory);
+categoryRouter.patch("/:id", updateCategory);
+categoryRouter.delete("/:id", removeCategory);
 
 export default categoryRouter;
