4a800bc287d9c60f44b8c9a6b3accf90e7cca751
hoangnm.ndm <hoangnm.ndm@gmail.com>
2023-11-24 18:06:30 +0700

    add valid custom

--- src/validations/productValid.js
+++ src/validations/productValid.js
@@ -1,7 +1,20 @@
 import Joi from "joi";
 
 export const productValid = Joi.object({
-  name: Joi.string().required().min(6).max(255),
-  price: Joi.number().required().min(0),
-  desc: Joi.string().min(6),
+  name: Joi.string().required().min(6).max(255).messages({
+    "string.base": "Name phải là một chuỗi!",
+    "string.empty": "Name không được để trống!",
+    "string.min": "Name phải có ít nhất 6 ký tự!",
+    "string.max": "Name không được quá 255 ký tự!",
+  }),
+  price: Joi.number().required().min(0).messages({
+    "number.base": "Price phải là một số!",
+    "number.empty": "Price không được để trống!",
+    "number.min": "Price phải lớn hơn 0!",
+  }),
+  desc: Joi.string().min(6).messages({
+    "string.base": "Desc phải là một chuỗi!",
+    "string.empty": "Desc không được để trống!",
+    "string.min": "Desc phải có ít nhất 6 ký tự!",
+  }),
 });
--- src/validations/userValid.js
+++ src/validations/userValid.js
@@ -1,13 +1,35 @@
 import Joi from "joi";
 
 export const signUpValid = Joi.object({
-  username: Joi.string().required().min(6).max(255),
-  email: Joi.string().email().required(),
-  password: Joi.string().required().min(6).max(255),
-  confirmPass: Joi.string().required().valid(Joi.ref("password")),
+  username: Joi.string().required().min(6).max(255).messages({
+    "string.base": "Username phải là một chuỗi!",
+    "string.empty": "Username không được để trống!",
+    "string.min": "Username phải có ít nhất 6 ký tự!",
+    "string.max": "Username không được quá 255 ký tự!",
+  }),
+  email: Joi.string().email().required().messages({
+    "string.empty": "Email không được để trống!",
+    "string.email": "Email không đúng định dạng!",
+  }),
+  password: Joi.string().required().min(6).max(255).messages({
+    "string.empty": "Password không được để trống!",
+    "string.min": "Password phải có ít nhất 6 ký tự!",
+    "string.max": "Password không được quá 255 ký tự!",
+  }),
+  confirmPass: Joi.string().required().valid(Joi.ref("password")).messages({
+    "string.empty": "Confirm Password không được để trống!",
+    "any.only": "Confirm Password không trùng khớp!",
+  }),
 });
 
 export const signInValid = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().required().min(6).max(255),
+  email: Joi.string().email().required().messages({
+    "string.empty": "Email không được để trống!",
+    "string.email": "Email không đúng định dạng!",
+  }),
+  password: Joi.string().required().min(6).max(255).messages({
+    "string.empty": "Password không được để trống!",
+    "string.min": "Password phải có ít nhất 6 ký tự!",
+    "string.max": "Password không được quá 255 ký tự!",
+  }),
 });
